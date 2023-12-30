e22a8fa6f8e521eced1f1db793122647953beede
delwarJnu11 <delwar999393@gmail.com>
2023-12-30 23:22:43 +0600

    add kib image

--- library_management/settings.py
+++ library_management/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -131,8 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'books/media/uploads/']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 MEDIA_URL = '/media/'
 LOGIN_URL = '/accounts/login/'
 
